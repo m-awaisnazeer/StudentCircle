bbc2cb42dc6acc36b7637d894a16c7af35ad1f32
Awais <m.awaisnaxeer@gmail.com>
2021-06-05 07:47:16 +0500

    List Users

---
+++ app/src/main/java/com/communisolve/studentcircle/adapter/UsersAdapter.java
@@ -0,0 +1,56 @@
+package com.communisolve.studentcircle.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.communisolve.studentcircle.Model.UserModel;
+import com.communisolve.studentcircle.databinding.LayoutUserItemBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> {
+    private LayoutUserItemBinding binding;
+
+    private ArrayList<UserModel> userModels;
+    private Context context;
+
+    public UsersAdapter(ArrayList<UserModel> userModels, Context context) {
+        this.userModels = userModels;
+        this.context = context;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public UsersAdapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        binding = LayoutUserItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull UsersAdapter.ViewHolder holder, int position) {
+        UserModel userModel = userModels.get(position);
+        Glide.with(context).load(userModel.getImage()).into(binding.userProfileImage);
+        binding.username.setText(userModel.getName());
+        binding.email.setText(userModel.getEmail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userModels.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/communisolve/studentcircle/ui/search/SearchFragment.java
+++ app/src/main/java/com/communisolve/studentcircle/ui/search/SearchFragment.java
@@ -4,21 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.communisolve.studentcircle.Model.UserModel;
+import com.communisolve.studentcircle.adapter.UsersAdapter;
 import com.communisolve.studentcircle.databinding.FragmentSearchBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import static com.communisolve.studentcircle.utils.Constants.USER_REF;
 
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
     private FragmentSearchBinding binding;
+
+    FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
+    private ArrayList<UserModel> userModels;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +46,32 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        mAuth = FirebaseAuth.getInstance();
+        UserRef = FirebaseDatabase.getInstance().getReference();
+
+        binding.searchUserRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        binding.searchUserRecyclerView.setLayoutManager(linearLayoutManager);
+        binding.searchUserRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), linearLayoutManager.getOrientation()));
+
+        UserRef.child(USER_REF).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    userModels = new ArrayList();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        userModels.add(dataSnapshot.getValue(UserModel.class));
+                    }
+
+                    binding.searchUserRecyclerView.setAdapter(new UsersAdapter(userModels, getContext()));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Toast.makeText(getContext(), "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return root;
     }
